74358a53e0bf34349e1ec7aac743b6c1e9fed376
Nicogz19 <niicogonzalez98@gmail.com>
2022-09-20 17:10:01 -0300

    I modify the html and css

--- js/calculadora.js
+++ js/calculadora.js
@@ -13,7 +13,6 @@ function sumar() {
 }
 
 function restar() {
-
     var n1 = parseFloat(document.getElementById("n1").value),
         n2 = parseFloat(document.getElementById("n2").value),
         r = document.getElementById("resultado");
@@ -21,7 +20,6 @@ function restar() {
 }
 
 function multiplicar() {
-
     var n1 = parseFloat(document.getElementById("n1").value),
         n2 = parseFloat(document.getElementById("n2").value),
         r = document.getElementById("resultado");
@@ -29,7 +27,6 @@ function multiplicar() {
 }
 
 function dividir() {
-
     var n1 = parseFloat(document.getElementById("n1").value),
         n2 = parseFloat(document.getElementById("n2").value),
         r = document.getElementById("resultado");
@@ -37,20 +34,18 @@ function dividir() {
 }
 
 function factorial(){
-
     var n1 = n2 = document.getElementById("n1").value;
 
     while (n1>1) {
-            n2 = n2 * (n1-1);
-            n1--;
-        }
+        n2 = n2 * (n1-1);
+        n1--;
+    }
     r = document.getElementById("resultado");
     r.innerHTML = n2
 
 }
 
 function cuadrado(){
-
     var n1 = parseFloat(document.getElementById("n1").value),
         r = document.getElementById("resultado");
 
